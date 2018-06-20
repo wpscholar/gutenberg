43cfca21364eae4be0933a60e585dccf84c8529d
iseulde <wp@iseulde.com>
2018-06-20 11:47:08 +0200

    Try with data layer

--- editor/store/actions.js
+++ editor/store/actions.js
@@ -718,3 +718,18 @@ export function updateEditorSettings( settings ) {
 		settings,
 	};
 }
+
+export function registerToken( name, settings ) {
+	return {
+		type: 'REGISTER_TOKEN',
+		name,
+		settings,
+	};
+}
+
+export function unregisterToken( name ) {
+	return {
+		type: 'UNREGISTER_TOKEN',
+		name,
+	};
+}
